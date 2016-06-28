31b3c3a36086bbd156300ddca313eda58b3388ae
maryam alghannam <maryamgh@berkeley.edu>
2016-06-28 17:03:41 +0100

    adding src and include

---
+++ include/surfaces.h
@@ -0,0 +1,20 @@
+#ifndef READMESH_SURFACES
+#define READMESH_SURFACES
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+class MySurfaces {
+  public: 
+  
+  private: 
+
+};
+
+#endif // READMESH_SURFACES
