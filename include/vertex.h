af66238b852df16202308ec277d237c56084256d
maryam alghannam <maryamgh@berkeley.edu>
2016-06-29 11:41:19 +0100

    removed public variables, default constructor, id reassignments, and fixed id type

--- include/vertex.h
+++ include/vertex.h
@@ -1,38 +1,28 @@
 #ifdef READMESH_VERTEX
 #define READMESH_VERTEX
 
-#include <vector>
 #include <array>
 
+// Vertex class
+// \brief Vertex for element generation 
+
 class Vertex {
+ public: 
+  // Constructor with coordinates and id
+  Vertex(const std::array<double, 3>& vcoord, const unsigned& vid): vid_{vid}, vcoord_{vcoord}{}
   
-  private: 
-   // creating private variables for coordinates and id  
-    std::array<double, 3> vcoord_;
-    std::vector<unsigned> vid_; 
-
-  public: 
-   // creating public variables for coordinates and id 
-    std::array<double, 3> vcoord;
-    std::vector<unsigned> vid; 
-    
-   // Default constructor 
-    Vertex(){};
-
-   // Constructor with coordinates and id
-    Vertex(const std::array<double, 3>& vcoord, const std::vector<unsigned>& vid): vid_{vid}, vcoord_{vcoord}{}
-  
-   // Assign coordinates to the vertex
-    void vcoord(const std::array<double, 3>& vcoord) {vcoord_ = vcoord; }
+  // Assign coordinates to the vertex
+  void vcoord(const std::array<double, 3>& vcoord) {vcoord_ = vcoord; }
    
-   // Assign id to the vertex
-    void vid(const std::vector<unsigned>& vid) {vid_ = vid; } 
-   
-   // Return coordinates 
-    std::array<double, 3> vcoord() const {return vcoord_; }
+  // Return coordinates of the vertex
+  std::array<double, 3> vcoord() const {return vcoord_; }
 
-   // Return id
-    std::vector<unsigned> vid() const {return vid_; }
+  // Return id of the vertex
+  unsigned vid() const {return vid_; }
+
+ private:  
+  std::array<double, 3> vcoord_;
+  unsigned vid_;
 
 };
 
