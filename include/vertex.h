750c0c2bbfc87e7ac9d4474b1f66ab847da7ca5b
maryam alghannam <maryamgh@berkeley.edu>
2016-06-29 12:14:19 +0100

    defining dimention as constant instead

--- include/vertex.h
+++ include/vertex.h
@@ -6,22 +6,24 @@
 // Vertex class
 // \brief Vertex for element generation 
 
+const unsigned Dim = 3;
+
 class Vertex {
  public: 
   // Constructor with coordinates and id
-  Vertex(const std::array<double, 3>& vcoord, const unsigned& vid): vid_{vid}, vcoord_{vcoord}{}
+  Vertex(const std::array<double, Dim>& vcoord, const unsigned& vid): vid_{vid}, vcoord_{vcoord}{}
   
   // Assign coordinates to the vertex
-  void vcoord(const std::array<double, 3>& vcoord) {vcoord_ = vcoord; }
+  void vcoord(const std::array<double, Dim>& vcoord) {vcoord_ = vcoord; }
    
   // Return coordinates of the vertex
-  std::array<double, 3> vcoord() const {return vcoord_; }
+  std::array<double, Dim> vcoord() const {return vcoord_; }
 
   // Return id of the vertex
   unsigned vid() const {return vid_; }
 
- private:  
-  std::array<double, 3> vcoord_;
+ private: 
+  std::array<double, Dim> vcoord_;
   unsigned vid_;
 
 };
