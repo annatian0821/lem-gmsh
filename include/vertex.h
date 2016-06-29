7fa902129c7a93abd2f6f845c7124ecd5da1acb5
maryam alghannam <maryamgh@berkeley.edu>
2016-06-29 10:37:28 +0100

    updating vertex.h

--- include/vertex.h
+++ include/vertex.h
@@ -5,9 +5,10 @@
 #include <array>
 
 class Vertex {
+  
   private: 
    // creating private variables for coordinates and id  
-    std::array<double,3> vcoord_;
+    std::array<double, 3> vcoord_;
     std::vector<unsigned> vid_; 
 
   public: 
@@ -18,23 +19,20 @@ class Vertex {
    // Default constructor 
     Vertex(){};
 
-   // Constructor with id and coordinates
-    Vertex(const std::vector<unsigned>& vid, const std::array<double, 3>& vcoord): vid_{vid}, vcoord_{vcoord}{}
-
+   // Constructor with coordinates and id
+    Vertex(const std::array<double, 3>& vcoord, const std::vector<unsigned>& vid): vid_{vid}, vcoord_{vcoord}{}
+  
+   // Assign coordinates to the vertex
+    void vcoord(const std::array<double, 3>& vcoord) {vcoord_ = vcoord; }
+   
+   // Assign id to the vertex
+    void vid(const std::vector<unsigned>& vid) {vid_ = vid; } 
+   
    // Return coordinates 
-    std::array<double,3> vcoord() const {return vcoord_; } 
+    std::array<double, 3> vcoord() const {return vcoord_; }
 
    // Return id
     std::vector<unsigned> vid() const {return vid_; }
-  
-   // Assign coordinates to the vertex
-    void vcoord(const std::array<double, 3>& vcoord) {
-      vcoord_ = vcoord; 
-      }
-   // Assign id to the vertex
-    void vid(const std::vector<unsigned>& vid) {
-      vid_ = vid; 
-    } 
 
 };
 
