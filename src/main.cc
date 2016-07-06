cb0f98e0dd9513f9e6e9bc43f50e68ded8d04e2a
maryam alghannam <maryamgh@berkeley.edu>
2016-07-06 18:10:53 +0100

    mesh reader for elements and vertices

--- src/main.cc
+++ src/main.cc
@@ -4,7 +4,7 @@
 #include "element.h"
 #include "surface.h"
 #include "vertex.h"
-
+#include "mesh.h" 
 
 int main(int argc, char** argv) {
   unsigned id = 0;
@@ -16,4 +16,9 @@ int main(int argc, char** argv) {
   std::cout << "Vertex id: " << vertex->id() << std::endl;
 
   auto surface = std::make_shared<Surface>(id);
+ 
+  // creat mesh object and read msh file
+  auto mesh = std::make_shared<Mesh>();
+  mesh->read_msh_file("mesh.msh");
+
 }
