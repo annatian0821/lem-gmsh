4ef8bfaf89d3c50a58917c09eaa14b09c0fa9745
Krishna Kumar <kks32@cam.ac.uk>
2017-02-22 16:59:50 +0000

    Exception handling in main

--- src/main.cc
+++ src/main.cc
@@ -9,25 +9,28 @@
 #include "vertex.h"
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    std::cout << "Usage: ./lem-gmsh /path/to/mesh_file.msh\n";
-    std::cerr << "Incorrect number of arguments\n";
-    std::exit(EXIT_FAILURE);
+  try {
+    if (argc != 2) {
+      std::cout << "Usage: ./lem-gmsh /path/to/mesh_file.msh\n";
+      throw std::runtime_error("Incorrect number of input arguments\n");
+    }
+
+    std::string filename = argv[1];
+
+    // Creat mesh object
+    std::unique_ptr<Mesh> mesh(new Mesh(1));
+
+    // Read msh file
+    mesh->read_msh_file(filename);
+
+    // Print out fracture pairs and nodes coordinates
+    mesh->write_fracture_pairs();
+
+    mesh->align_nodes_on_plane("Fracture", 1);
+
+    //  mesh->align_nodes_on_plane("WeakPlane", 1);
+    mesh->write_nodes();
+  } catch (std::exception& except) {
+    std::cout << "Caught exception: " << except.what() << '\n';
   }
-
-  std::string filename = argv[1];
-
-  // Creat mesh object
-  std::unique_ptr<Mesh> mesh(new Mesh(1));
-
-  // Read msh file
-  mesh->read_msh_file(filename);
-
-  // Print out fracture pairs and nodes coordinates
-  mesh->write_fracture_pairs();
-
-  mesh->align_nodes_on_plane("Fracture", 1);
-
-  //  mesh->align_nodes_on_plane("WeakPlane", 1);
-  mesh->write_nodes();
 }
